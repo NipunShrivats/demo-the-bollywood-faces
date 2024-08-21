53eda39d3a97bee54fbf4b57a92c2d4fe7d49fda
Nipun Shrivats <nipunrawat16@gmail.com>
2024-08-21 10:17:42 +0530

    changes on 21st Aug commit 1

--- src/Components/SuccessCards/SuccessCards.jsx
+++ src/Components/SuccessCards/SuccessCards.jsx
@@ -3,15 +3,18 @@ import "./SuccessCardsStyle.css";
 import { Link } from "react-router-dom"
 
 import success1 from "../../assets/success/1.png";
+import success2 from "../../assets/success/2.png";
+import success3 from "../../assets/success/3.png";
+import success4 from "../../assets/success/4.png";
+import success5 from "../../assets/success/5.png";
+import success6 from "../../assets/success/6.png";
 
 function Card(props) {
     const { src, title, to } = props
     return (
         <>
             <div className='issueCard'>
-                <Link to={to} target='blank_'>
-                    <img src={src} alt={src} className='image' />
-                </Link>
+                <img src={src} alt={src} className='image' />
                 <div className='bottom-box'>
                     <p>{title}</p>
                 </div>
@@ -25,11 +28,11 @@ export default function IssueCards() {
         <>
             <div className="issue">
                 <Card src={success1} />
-                <Card src={success1} />
-                <Card src={success1} />
-                <Card src={success1} />
-                <Card src={success1} />
-                <Card src={success1} />
+                <Card src={success2} />
+                <Card src={success3} />
+                <Card src={success4} />
+                <Card src={success5} />
+                <Card src={success6} />
             </div>
         </>
     )
