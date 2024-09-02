27eaa227bed83feebed416a92303a4db2be32fff
NipunShrivats <nipunrawat16@gmail.com>
2024-09-02 18:50:38 +0530

    changes on 2sep, commit no.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,32 @@
-import React, { useState, useEffect } from 'react'
-import Footer from './Components/Footer/Footer'
-import Home from './Pages/Home/Home'
-import PrivacyPolicy from './Pages/PrivacyPolicy/PrivacyPolicy'
-import Contact from './Components/Contact/Contact'
-import Title from './Components/Title/Title'
-import Issues from './Pages/Issues/Issues'
-import Success from './Pages/Success/Success'
-
+import React, { useState, useEffect } from "react";
+import Footer from "./Components/Footer/Footer";
+import Home from "./Pages/Home/Home";
+import Policy from "./Pages/Policy/Policy";
+import Contact from "./Components/Contact/Contact";
+import Title from "./Components/Title/Title";
+import Issues from "./Pages/Issues/Issues";
+import Success from "./Pages/Success/Success";
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 export default function App() {
   return (
     <>
-
       <BrowserRouter>
         <Routes>
-          <Route path='/' exact={true} element={<Home />} />
-          <Route path='/PrivacyPolicy' exact={true} element={<PrivacyPolicy />} />
-          <Route path='/Issues' exact={true} element={<Issues />} />
-          <Route path='/Successstories' exact={true} element={<Success />} />
+          <Route path="/" exact={true} element={<Home />} />
+          <Route path="/PrivacyPolicy" exact={true} element={<Policy />} />
+          <Route path="/Issues" exact={true} element={<Issues />} />
+          <Route path="/Successstories" exact={true} element={<Success />} />
         </Routes>
-        <div className='container'>
+        <div className="container">
           <Title title={"contact us"} subtitle={"get in touch"} />
           <Contact />
         </div>
-        <div className='container'>
+        <div className="container">
           <Footer />
         </div>
       </BrowserRouter>
-
     </>
-
-  )
+  );
 }
