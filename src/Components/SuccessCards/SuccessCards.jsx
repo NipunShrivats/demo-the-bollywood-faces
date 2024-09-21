e282bb9ba0a9fe24e7370b18ab038c2ce4ab4f93
NipunShrivats <nipunrawat16@gmail.com>
2024-09-21 12:09:01 +0530

    changes on 21sep, commit no.11

--- src/Components/SuccessCards/SuccessCards.jsx
+++ src/Components/SuccessCards/SuccessCards.jsx
@@ -1,39 +1,54 @@
-import React from 'react';
+import React from "react";
 import "./SuccessCardsStyle.css";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
-import success1 from "../../assets/success/1.png";
-import success2 from "../../assets/success/2.png";
-import success3 from "../../assets/success/3.png";
-import success4 from "../../assets/success/4.png";
-import success5 from "../../assets/success/5.png";
-import success6 from "../../assets/success/6.png";
+// import success1 from "../../assets/success/1.png";
+// import success2 from "../../assets/success/2.png";
+// import success3 from "../../assets/success/3.png";
+// import success4 from "../../assets/success/4.png";
+// import success5 from "../../assets/success/5.png";
+// import success6 from "../../assets/success/6.png";
+
+import sWall1 from "../../assets/success/wall-1.png";
+import sWall2 from "../../assets/success/wall-2.png";
+import sWall3 from "../../assets/success/wall-3.png";
+import sWall4 from "../../assets/success/wall-4.png";
+import sWall5 from "../../assets/success/wall-5.png";
+import sWall6 from "../../assets/success/wall-6.png";
+import sWall7 from "../../assets/success/wall-7.png";
 
 function Card(props) {
-    const { src, title } = props
-    return (
-        <>
-            <div className='successCard'>
-                <img src={src} alt={src} className='image stiff-block' />
-                <div className='bottom-box'>
-                    <p>{title}</p>
-                </div>
-            </div>
-        </>
-    )
+  const { src, title } = props;
+  return (
+    <>
+      <div className="successCard">
+        <img src={src} alt={src} className="image stiff-block" />
+        <div className="bottom-box">
+          <p>{title}</p>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default function IssueCards() {
-    return (
-        <>
-            <div className="success">
-                <Card src={success1} />
-                <Card src={success2} />
-                <Card src={success3} />
-                <Card src={success4} />
-                <Card src={success5} />
-                <Card src={success6} />
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="success">
+        <Card src={sWall1} />
+        <Card src={sWall2} />
+        <Card src={sWall3} />
+        <Card src={sWall4} />
+        <Card src={sWall5} />
+        <Card src={sWall6} />
+        <Card src={sWall7} />
+        {/* <Card src={success1} />
+        <Card src={success2} />
+        <Card src={success3} />
+        <Card src={success4} />
+        <Card src={success5} />
+        <Card src={success6} /> */}
+      </div>
+    </>
+  );
 }
--- src/Pages/Success/Success.jsx
+++ src/Pages/Success/Success.jsx
@@ -1,50 +1,46 @@
-import { React, useState, useEffect } from 'react'
-import "./SuccessStyle.css"
-import Nav from "../../Components/Navbar/Navbar"
-import HeroSuccess from "../../Components/HeroSuccess/HeroSuccess"
-import SuccessCards from "../../Components/SuccessCards/SuccessCards"
-import Title from "../../Components/Title/Title"
+import { React, useState, useEffect } from "react";
+import "./SuccessStyle.css";
+import Nav from "../../Components/Navbar/Navbar";
+import HeroSuccess from "../../Components/HeroSuccess/HeroSuccess";
+import SuccessCards from "../../Components/SuccessCards/SuccessCards";
+import Title from "../../Components/Title/Title";
 
 import BeatLoader from "react-spinners/BeatLoader";
 export default function Success() {
+  const [playState, setPlayState] = useState(false);
 
-    const [playState, setPlayState] = useState(false);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
 
-    const [loading, setLoading] = useState(false);
-    useEffect(() => {
-        setLoading(true)
-        setTimeout(() => {
-            setLoading(false)
-        }, 1000);
-    }, [])
-
-
-    return (
+  return (
+    <>
+      {loading ? (
+        <div className="loader-cover">
+          <BeatLoader
+            color={"rgb(232, 151, 0)"}
+            loading={loading}
+            size={20}
+            aria-label="Loading bouncer"
+            data-testid="loader"
+            className="loader"
+          />
+        </div>
+      ) : (
         <>
-            {
-                loading ?
-                    <div className='loader-cover'>
-                        <BeatLoader
-                            color={"rgb(232, 151, 0)"}
-                            loading={loading}
-                            size={20}
-                            aria-label="Loading bouncer"
-                            data-testid="loader"
-                            className='loader'
-                        />
-                    </div>
-                    :
-                    <>
-                        <Nav />
-                        <HeroSuccess />
+          <Nav />
+          <HeroSuccess />
 
-                        <div className='container'>
-                            <Title title={"stories"} subtitle={"view our success stories"} />
-                            <SuccessCards />
-                        </div>
-
-                    </>
-            }
+          <div className="container">
+            <Title title={"stories"} subtitle={"view our success stories"} />
+            <SuccessCards />
+          </div>
         </>
-    )
+      )}
+    </>
+  );
 }
