8ebf5f498987ef6e6f634e229c307c10a9846cbe
NipunShrivats <nipunrawat16@gmail.com>
2024-09-21 10:46:09 +0530

    changes on 21sep, commit no.2

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,25 +1,33 @@
-import React from 'react';
+import React from "react";
 import "./HeroStyle.css";
 
 import { FaWhatsapp } from "react-icons/fa";
 
 export default function Hero() {
-    return (
-        <>
-            <div className='hero container'>
-                <div className='hero-text hero-block'>
-                    <h1>welcome</h1>
-                    <h1 className='middle-line'>to</h1>
-                    <h1 className='last-line'>the bollywood faces</h1>
-                    <p>"The Bollywood Faces" provides an opportunity for networking with industry stalwart and socialite. </p>
-                    <a href="https://wa.me/918800348485?text=Hello, I want to know more about your services" target='blank_'>
-                        <button className='btn btn-wp'>
-                            <span><FaWhatsapp /></span>
-                            Chat on whatsapp
-                        </button>
-                    </a>
-                </div>
-            </div >
-        </>
-    )
+  return (
+    <>
+      <div className="hero container">
+        <div className="hero-text hero-block">
+          <h1>welcome</h1>
+          <h1 className="middle-line">to</h1>
+          <h1 className="last-line">the bollywood faces</h1>
+          <p>
+            "The Bollywood Faces" provides an opportunity for networking with
+            industry stalwart and socialite.{" "}
+          </p>
+          <a
+            href="https://wa.me/918800348485?text=Hello, I want to know more about your services"
+            target="blank_"
+          >
+            <button className="btn btn-wp">
+              <span>
+                <FaWhatsapp />
+              </span>
+              Chat on whatsapp
+            </button>
+          </a>
+        </div>
+      </div>
+    </>
+  );
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,68 +1,89 @@
-import React, { useEffect, useState } from 'react'
-import "./NavbarStyle.css"
-import logow from "../../assets/imgAssets/logo-white.png"
-import logob from "../../assets/imgAssets/logo-black.png"
+import React, { useEffect, useState } from "react";
+import "./NavbarStyle.css";
+import logow from "../../assets/imgAssets/logo-white.png";
+import logob from "../../assets/imgAssets/logo-black.png";
+import logoMain from "../../assets/imgAssets/bollywoodfaces.png";
 
-import { Link, Button, Element, Events, animateScroll as scroll, scrollSpy } from 'react-scroll';
+import {
+  Link,
+  Button,
+  Element,
+  Events,
+  animateScroll as scroll,
+  scrollSpy,
+} from "react-scroll";
 
 // import { Link } from "react-router-dom"
 import { RxHamburgerMenu } from "react-icons/rx";
 
-import MenuIcon from "../../assets/imgAssets/menu-icon.png"
+import MenuIcon from "../../assets/imgAssets/menu-icon.png";
 
 export default function Navbar() {
+  const [sticky, setSticky] = useState(false);
 
-    const [sticky, setSticky] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      window.scrollY > 100 ? setSticky(true) : setSticky(false);
+    });
+  }, []);
 
-    useEffect(() => {
-        window.addEventListener("scroll", () => {
-            window.scrollY > 100 ? setSticky(true) : setSticky(false);
-        })
-    }, [])
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const toggleMenu = () => {
+    mobileMenu ? setMobileMenu(false) : setMobileMenu(true);
+  };
 
-    const [mobileMenu, setMobileMenu] = useState(false);
-    const toggleMenu = () => {
-        mobileMenu ? setMobileMenu(false) : setMobileMenu(true)
-    }
+  return (
+    <>
+      <nav className={`container ${sticky ? "dark-nav" : ""}`}>
+        <a href="/">
+          {/* <div className="logo-cover1">
+            <div className="logo-inside1">
+              <img src={logow} className="logo" alt="" />
+            </div>
 
-    return (
-        <>
-            <nav className={`container ${sticky ? 'dark-nav' : ''}`}>
-                <a href="/">
-                    <div className='logo-cover1'>
-                        <div className='logo-inside1'>
-                            <img src={logow} className='logo' alt="" />
-                        </div>
+            <div className="logo-inside2">
+              <h5>SENSATIONZ</h5>
+              <h3>The Bollywood Faces</h3>
+            </div>
+          </div> */}
+          <img src={logoMain} alt="" className="logo-main" />
+        </a>
+        <img src={MenuIcon} alt="" className="menu-icon" onClick={toggleMenu} />
 
-                        <div className='logo-inside2'>
-                            <h5>SENSATIONZ</h5>
-                            <h3>The Bollywood Faces</h3>
-                        </div>
-                    </div>
-                </a>
-                <img src={MenuIcon} alt="" className='menu-icon' onClick={toggleMenu} />
+        <ul className={mobileMenu ? "" : "hide-mobile-menu"}>
+          <li>
+            <a href="/">Home</a>
+          </li>
 
-                <ul className={mobileMenu ? "" : "hide-mobile-menu"}>
+          <li>
+            <a href="/Issues">Issues</a>
+          </li>
 
-                    <li><a href='/'>Home</a></li>
+          <li>
+            <a href="/Successstories">Success Stories</a>
+          </li>
 
-                    <li><a href="/Issues">Issues</a></li>
+          <li>
+            <Link to="magazine" smooth={true} offset={-230} durastion={500}>
+              Magazine release
+            </Link>
+          </li>
 
-                    <li><a href="/Successstories">Success Stories</a></li>
+          {/* <li><a href="#magazine">Magazine release</a></li> */}
 
-                    <li><Link to='magazine'
-                        smooth={true}
-                        offset={-230}
-                        durastion={500}>Magazine release</Link></li>
-
-                    {/* <li><a href="#magazine">Magazine release</a></li> */}
-
-                    <li className='btn-li'><Link to='contact'
-                        smooth={true}
-                        offset={-250}
-                        durastion={500} className='btn dark-btn'>Contact Us</Link> </li>
-                </ul>
-            </nav >
-        </>
-    )
+          <li className="btn-li">
+            <Link
+              to="contact"
+              smooth={true}
+              offset={-250}
+              durastion={500}
+              className="btn dark-btn"
+            >
+              Contact Us
+            </Link>{" "}
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
 }
