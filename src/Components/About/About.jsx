00c20a2ce8add827c4bd70c17869a7ce7cdebaed
NipunShrivats <nipunrawat16@gmail.com>
2024-09-21 11:32:29 +0530

    changes on 21sep, commit no.6

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,28 +1,52 @@
-import React from 'react';
+import React from "react";
 import "./AboutStyle.css";
-import aboutImg from "../../assets/imgAssets/about.jpg"
-import playIcon from "../../assets/imgAssets/play-icon.png"
-
+import aboutImg from "../../assets/imgAssets/about.jpg";
+import playIcon from "../../assets/imgAssets/play-icon.png";
 
 export default function About({ setPlayState }) {
-    return (
-        <>
-            <div className='about'>
+  return (
+    <>
+      <div className="about">
+        <div className="about-right stiff-block">
+          {/* <h3>About The Bollywood Faces</h3> */}
+          <p>
+            The Bollywood Faces is a unit of Sensationz Media & Art Pvt. Ltd.
+            Covering various personalities related to segments like
+            Photographer, Designer, Makeup Artist, Makeover Artist, Production
+            Houses. In this context we have covered various personality such as
+            Varija Bajaj, Pooja Motwani, Rahul Dutta, Suvo Das, Meenakshi Dutta,
+            Gunjan Gaur, Akash K Agarwal and Lt. Rita gangwani in past issues.
+          </p>
 
-                <div className='about-right stiff-block'>
-                    <h3>About The Bollywood Faces</h3>
-                    <p>The Bollywood Faces is a unit of Sensationz Media & Art Pvt. Ltd. Covering various personalities related to segments like Photographer, Designer, Makeup Artist, Makeover Artist, Production Houses. In this context we have covered various personality such as Varija Bajaj, Pooja Motwani, Rahul Dutta, Suvo Das, Meenakshi Dutta, Gunjan Gaur, Akash K Agarwal and Lt. Rita gangwani in past issues.</p>
+          <p>
+            “The Bollywood Faces” also help the newcomers to craft their career
+            in the Modelling and Acting. This work as a interface between the
+            industry and budding talent, in this process we have produced many
+            stars who are shining in glamour world be it daily sops like “Yeh
+            Rishta Kya Kahalata Hai”, ” Yeh Vada Raha”, “Sasural Simar Ka” or ad
+            films, feature films are all there in our kitty.
+          </p>
 
-                    <p>“The Bollywood Faces” also help the newcomers to craft their career in the Modelling and Acting. This work as a interface between the industry and budding talent, in this process we have produced many stars who are shining in glamour world be it daily sops like “Yeh Rishta Kya Kahalata Hai”, ” Yeh Vada Raha”, “Sasural Simar Ka” or ad films, feature films are all there in our kitty.</p>
+          <p>
+            The Bollywood Faces’s unveiling ceremony is through an event which
+            provides an opportunity for networking with industry stalwart and
+            socialite. As this event revolve around a social issue, so it gives
+            the platform to fulfill the social responsibility also
+          </p>
+        </div>
 
-                    <p>The Bollywood Faces’s unveiling ceremony is through an event which provides an opportunity for networking with industry stalwart and socialite. As this event revolve around a social issue, so it gives the platform to fulfill the social responsibility also</p>
-                </div>
-
-                <div className='about-left stiff-block'>
-                    <img src={aboutImg} className='about-img' alt="" />
-                    <img src={playIcon} className='play-icon' alt="" onClick={() => { setPlayState(true) }} />
-                </div>
-            </div>
-        </>
-    )
+        <div className="about-left stiff-block">
+          <img src={aboutImg} className="about-img" alt="" />
+          {/* <img
+            src={playIcon}
+            className="play-icon"
+            alt=""
+            onClick={() => {
+              setPlayState(true);
+            }}
+          /> */}
+        </div>
+      </div>
+    </>
+  );
 }
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,63 +1,61 @@
-import React, { useState, useEffect } from 'react'
-import "./HomeStyle.css"
+import React, { useState, useEffect } from "react";
+import "./HomeStyle.css";
 
-import Navbar from '../../Components/Navbar/Navbar'
-import Title from '../../Components/Title/Title'
-import Hero from '../../Components/Hero/Hero'
-import About from '../../Components/About/About'
-import Contact from '../../Components/Contact/Contact'
-import VideoPlayer from '../../Components/VideoPlayer/VideoPlayer'
-import Magazine from '../../Components/Magazine/Magazine'
+import Navbar from "../../Components/Navbar/Navbar";
+import Title from "../../Components/Title/Title";
+import Hero from "../../Components/Hero/Hero";
+import About from "../../Components/About/About";
+import Contact from "../../Components/Contact/Contact";
+import VideoPlayer from "../../Components/VideoPlayer/VideoPlayer";
+import Magazine from "../../Components/Magazine/Magazine";
 
 import BeatLoader from "react-spinners/BeatLoader";
 
-
 export default function Home() {
+  const [playState, setPlayState] = useState(false);
 
-    const [playState, setPlayState] = useState(false);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
 
-    const [loading, setLoading] = useState(false);
-    useEffect(() => {
-        setLoading(true)
-        setTimeout(() => {
-            setLoading(false)
-        }, 1000);
-    }, [])
-
-
-    return (
+  return (
+    <>
+      {loading ? (
+        <div className="loader-cover">
+          <BeatLoader
+            color={"rgb(232, 151, 0)"}
+            loading={loading}
+            size={20}
+            aria-label="Loading bouncer"
+            data-testid="loader"
+            className="loader"
+          />
+        </div>
+      ) : (
         <>
-            {
-                loading ?
-                    <div className='loader-cover'>
-                        <BeatLoader
-                            color={"rgb(232, 151, 0)"}
-                            loading={loading}
-                            size={20}
-                            aria-label="Loading bouncer"
-                            data-testid="loader"
-                            className='loader'
-                        />
-                    </div>
-                    :
-                    <>
-                        <Navbar />
-                        <Hero />
+          <Navbar />
+          <Hero />
 
-                        <div className='container'>
-                            <Title title={"about Us"} subtitle={"know more about us"} />
-                            <About setPlayState={setPlayState} />
-                        </div>
+          <div className="container">
+            <Title title={"about Us"} subtitle={"know more about us"} />
+            <About setPlayState={setPlayState} />
+          </div>
 
-                        <div className='container'>
-                            <Title title={"Magazine Release"} subtitle={"release event gallery"} />
-                            < Magazine />
-                        </div>
+          <div className="container">
+            <Title
+              title={"Magazine Release"}
+              subtitle={"Magazine Release Event"}
+            />
+            <Magazine />
+          </div>
 
-                        <VideoPlayer playState={playState} setPlayState={setPlayState} />
-                    </>
-            }
+          <VideoPlayer playState={playState} setPlayState={setPlayState} />
         </>
-
-    )
+      )}
+    </>
+  );
 }
